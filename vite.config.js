9451573be009bd0548dca3e210091a1c55aceba9
abdulraheem6 <moise1445@gmail.com>
2026-06-03 10:27:25 +0530

    feat: changed base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tiktok-ad-feed/',
+  base: '/nearby-offers/',
   server: {
     port: 3000
   }
